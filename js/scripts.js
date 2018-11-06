e17d6111a93833e7d07a97451a55e7d776577e7c
bradwfresno <bradwfresno@gmail.com>
2018-11-05 20:32:49 -0800

    dayOne done

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var firstName = 'Brad';
+console.log(firstName);
+// I am a comment
+/* 
+Lots
+Of 
+Comments
+*/ 
+
+function(){
+    console.log('camelCase');
+    console.log('soethingNew');
+    if(true){
+        console.log('string');
+    }
+}
