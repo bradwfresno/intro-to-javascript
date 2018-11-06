edcc65f7bcc250991217176cb2754d93de8512dd
bradwfresno <bradwfresno@gmail.com>
2018-11-05 20:53:58 -0800

    dayOneTakehome done

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,6 @@
-var firstName = 'Brad';
-console.log(firstName);
-// I am a comment
-/* 
-Lots
-Of 
-Comments
-*/ 
+var hopesDreams = 'I hope to get out of this class a better understanding of accessing and changing client side data. I wish to learn to capture offline form data and upload when a connection is detected like an offline data collection app. I dont expect this to be something I will learn in this class but hope to eventually get there. I would also like to learn how to get the viewable area of the browser to send data to HTML elements.';
+var myHobby = 'I like the video game "Fallout" and everything about its lore and collectables.';
+console.log(hopesDreams);
+console.log(myHobby);
 
-function(){
-    console.log('camelCase');
-    console.log('soethingNew');
-    if(true){
-        console.log('string');
-    }
-}
+
