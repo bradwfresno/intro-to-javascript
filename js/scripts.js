33c9453779eb2b54a641d85b930d2e1abbbd50de
Brad Williams <bradwfresno@gmail.com>
2018-11-07 21:02:25 -0800

    dayThree cont

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const obj1 = {
+    name: "Brad"
+};
+const obj2 = {
+    name: "Brad"
+};
+const arr1 = ['Cat'];
+const arr2 = ['Cat'];
+
+console.log(obj1 == obj2)
+console.log(obj1.toString() == obj2.toString())
+
+console.log(arr1 == arr2)
+console.log(arr1.toString() == arr2.toString())
