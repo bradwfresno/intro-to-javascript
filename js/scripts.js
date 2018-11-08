1fac05d81daa33e73d609728307246b35f8cef5a
Brad Williams <bradwfresno@gmail.com>
2018-11-07 20:12:22 -0800

    dayTwo done

---
+++ js/scripts.js
@@ -0,0 +1,61 @@
+//String
+// var name = "Brad";
+//String Object
+// var nameTwo = new String("Williams");
+// console.log(nameTwo);
+//Number
+// var luckyNumber = 42;
+//Boolean
+// var boolean = true;
+//Undefined
+// var mystery;
+// console.log(typeof(mystery));
+//object
+
+//symbol
+// console.log(typeof(Symbol()));
+//Test Unit
+// console.log(name===nameTwo);
+// const name = "Brad";
+
+{
+    // const name = "Alana";
+    // console.log(name);
+}
+// console.log(name);
+
+// const greeting = "Hello my name is : ";
+const name = "Brad";
+
+console.log(`Lorem ipsum, ${name} dolor sit amet consectetur adipisicing elit. Minima dolorum vero quam eligendi reiciendis, impedit possimus aperiam autem similique cum voluptatum enim officiis sunt omnis cumque praesentium vel, adipisci  reprehenderit.`);
+
+const date = new Date('09/27/89');
+
+console.log(date);
+
+console.log('jasdfjhsdgfjshdgfkjhsadgfjshadgfjsadgfjshdgsjadhfg'.length);
+
+const num = new Number(42);
+const num2 = 42;
+console.log(num,num2);
+const array = [1,2,3,];
+console.log(array);
+
+const array2 = new Array(1,2,3);
+console.log(array2);
+
+const arrayMixed = ['Brad',36,true];
+console.log(arrayMixed);
+
+const obj = {
+    name: 'Brad',
+    age: 29
+};
+console.log(obj);
+const response = 
+//alert('this is an alert')
+confirm("are you sure?") ;//cancle / ok
+console.log(response);
+
+const faveMovie = prompt("Whats yoru fave movie?");
+console.log(name,faveMovie)
